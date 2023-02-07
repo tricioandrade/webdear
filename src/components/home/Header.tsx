03b3de7ed9dbb75262b1ba78d3bd3a5d42d1f7e2
Patrício Andrade <tricioandrade@outlook.com>
2023-02-07 08:54:14 -0300

    update Header

--- src/components/home/Header.tsx
+++ src/components/home/Header.tsx
@@ -6,15 +6,30 @@ import logo from '../../assets/logo.png';
 const Header = () => {
 
     return (
-            <Navbar bg="" expand="lg" variant='light' className='d-block fixed-top navbar-menu'>
+            <header  className='d-block fixed-top'>
+                <Navbar bg="" expand="lg" variant='light' className='d-block navbar-menu'>
+                    <Container>
+                        <Nav className="me-4 justify-content-start">
+                            <Nav.Item><Link to='#header' className='nav-link'><i className='fa fa-envelope'/> webdear@outlook.pt</Link></Nav.Item>
+                            <Nav.Item><Link to='#header' className='nav-link'><i className='fa fa-phone-square'/> +244 952 991 773</Link></Nav.Item>
+                            <Nav.Item><Link to='#header' className='nav-link'><i className='fab fa-'/></Link></Nav.Item>
+                        </Nav>
+                        <Nav className="me-4 justify-content-end">
+                            <Nav.Item><Link to='#header' className='nav-link'><i className='fab fa-facebook'/></Link></Nav.Item>
+                            <Nav.Item><Link to='#header' className='nav-link'><i className='fab fa-whatsapp'/></Link></Nav.Item>
+                            <Nav.Item><Link to='#header' className='nav-link'><i className='fab fa-instagram'/></Link></Nav.Item>
+                            <Nav.Item><Link to='#header' className='nav-link'><i className='fab fa-twitter'/></Link></Nav.Item>
+                        </Nav>
+                    </Container>
+                </Navbar>
+                <Navbar bg="" expand="lg" variant='light' className='d-block navbar-menu'>
                 <Container>
                     <Navbar.Brand href="#home"> 
                     <img src={logo} alt='webdear' /> 
                     </Navbar.Brand>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />        
-                    <Navbar.Collapse id="basic-navbar-nav" className='flex-row-reverse'>
-                        
-                        <Nav className="me-4">
+                    <Navbar.Collapse id="basic-navbar-nav" className='d-flex justify-content-end'>
+                        <Nav className="me-4 justify-content-end">
                            <Nav.Item><Link to='#header' className='nav-link'><i className='fa fa-home'/> Home</Link></Nav.Item>
                            <Nav.Item><Link to='#header' className='nav-link'><i className='fa fa-question-circle'/> Sobre</Link></Nav.Item>
                            <Nav.Item><Link to='#header' className='nav-link'><i className='fa fa-phone-alt'/> Contacto</Link></Nav.Item>
@@ -23,6 +38,7 @@ const Header = () => {
                     </Navbar.Collapse>
                 </Container>
             </Navbar>
+            </header>
     )
 }
 
